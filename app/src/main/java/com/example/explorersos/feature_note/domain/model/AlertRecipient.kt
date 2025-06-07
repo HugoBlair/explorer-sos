dfd8b12e24b81833dc492c0d1dd30f42c1fb5c65
Hugo <hugorosa123@gmail.com>
2025-06-06 18:00:59 -0600

    Added init exception checking to alertRecipient

--- app/src/main/java/com/example/explorersos/feature_note/domain/model/AlertRecipient.kt
+++ app/src/main/java/com/example/explorersos/feature_note/domain/model/AlertRecipient.kt
@@ -1,5 +1,6 @@
 package com.example.explorersos.feature_note.domain.model
 
+import android.util.Patterns
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,8 +8,20 @@ import androidx.room.PrimaryKey
 data class AlertRecipient(
     @PrimaryKey val id: Int? = null,
     val recipientEmail: String?,
-    val recipientPhone: Int?
-    )
+    val recipientPhone: String?
+    ){
+    init {
+        if (recipientEmail.isNullOrBlank() && recipientPhone.isNullOrBlank()) {
+            throw InvalidAlertRecipientException("Either email or phone must be provided.")
+        }
+        if (recipientEmail != null && !Patterns.EMAIL_ADDRESS.matcher(recipientEmail).matches()) {
+            throw InvalidAlertRecipientException("Invalid email format.")
+        }
+        if(recipientPhone!= null && !Patterns.PHONE.matcher(recipientPhone).matches()) {
+            throw InvalidAlertRecipientException("Invalid phone number format.")
+        }
+    }
+}
 
-class invalidAlertRecipientException(message: String): Exception(message)
+class InvalidAlertRecipientException(message: String): Exception(message)
 
