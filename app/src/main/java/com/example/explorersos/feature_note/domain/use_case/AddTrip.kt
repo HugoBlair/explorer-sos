e36ac8f90dfb9de61bffec0fb89d1b1779f7837d
Hugo <hugorosa123@gmail.com>
2025-06-07 19:33:47 -0600

    Implemented AddTrip use case

--- app/src/main/java/com/example/explorersos/feature_note/domain/use_case/AddTrip.kt
+++ app/src/main/java/com/example/explorersos/feature_note/domain/use_case/AddTrip.kt
@@ -1,4 +1,20 @@
 package com.example.explorersos.feature_note.domain.use_case
 
-class AddTrip {
+import com.example.explorersos.feature_note.domain.model.Trip
+import com.example.explorersos.feature_note.domain.respository.TripRepository
+
+class AddTrip(
+    private val repository: TripRepository
+) {
+    @Throws(Trip.InvalidTripException::class)
+    suspend operator fun invoke(trip: Trip) {
+        if (trip.title.isBlank()) {
+            throw Trip.InvalidTripException("The title of the trip can't be empty.")
+        }
+        if (trip.description.isBlank()) {
+            throw Trip.InvalidTripException("The content of the trip can't be empty.")
+
+        }
+        repository.insertTrip(trip)
+    }
 }
