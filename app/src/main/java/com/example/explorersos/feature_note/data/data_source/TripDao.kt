a678e053f27cdf79e7fd4818aaf11eac204ab30f
Hugo <hugorosa123@gmail.com>
2025-06-07 18:51:36 -0600

    Implemented TripDao.kt

--- app/src/main/java/com/example/explorersos/feature_note/data/data_source/TripDao.kt
+++ app/src/main/java/com/example/explorersos/feature_note/data/data_source/TripDao.kt
@@ -1,4 +1,24 @@
 package com.example.explorersos.feature_note.data.data_source
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.explorersos.feature_note.domain.model.Trip
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface TripDao {
+    @Query("SELECT * FROM trip")
+    fun getTrips(): Flow<List<Trip>>
+
+    @Query("SELECT * FROM trip WHERE id = :id")
+    suspend fun getTripById(id: Int): Trip?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrip(trip: Trip)
+
+    @Delete
+    suspend fun deleteTrip(trip: Trip)
 }
