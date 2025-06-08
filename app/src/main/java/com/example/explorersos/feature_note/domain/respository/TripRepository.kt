8f5faa96dca408d57d768e1da8a227d582f6a90e
Hugo <hugorosa123@gmail.com>
2025-06-07 18:49:36 -0600

    Implemented TripRepository.kt

--- app/src/main/java/com/example/explorersos/feature_note/domain/respository/TripRepository.kt
+++ app/src/main/java/com/example/explorersos/feature_note/domain/respository/TripRepository.kt
@@ -1,4 +1,15 @@
 package com.example.explorersos.feature_note.domain.respository
 
-class TripRepository {
+import com.example.explorersos.feature_note.domain.model.Trip
+import kotlinx.coroutines.flow.Flow
+
+interface TripRepository {
+
+    fun getTrips(): Flow<List<Trip>>
+
+    suspend fun getTripById(id: Int): Trip?
+
+    suspend fun insertTrip(trip: Trip)
+
+    suspend fun deleteTrip(trip: Trip)
 }
