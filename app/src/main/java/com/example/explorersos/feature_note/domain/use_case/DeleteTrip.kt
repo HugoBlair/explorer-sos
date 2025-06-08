e42b41ffad2a00aaa016e2d4c31a566e34da1ed4
Hugo <hugorosa123@gmail.com>
2025-06-07 19:34:03 -0600

    Implemented DeleteTrip use case

--- app/src/main/java/com/example/explorersos/feature_note/domain/use_case/DeleteTrip.kt
+++ app/src/main/java/com/example/explorersos/feature_note/domain/use_case/DeleteTrip.kt
@@ -1,4 +1,13 @@
 package com.example.explorersos.feature_note.domain.use_case
 
-class DeleteTrip {
+import com.example.explorersos.feature_note.domain.model.Trip
+import com.example.explorersos.feature_note.domain.respository.TripRepository
+
+class DeleteTrip(
+    private val repository: TripRepository
+) {
+    suspend operator fun invoke(trip: Trip) {
+        repository.deleteTrip(trip)
+    }
+
 }
