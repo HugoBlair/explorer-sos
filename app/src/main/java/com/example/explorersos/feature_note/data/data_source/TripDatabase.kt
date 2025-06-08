a44097ec6cf0274060d40c143f4629f097a067bc
Hugo <hugorosa123@gmail.com>
2025-06-07 19:09:42 -0600

    Implemented TripDatabase.kt

--- app/src/main/java/com/example/explorersos/feature_note/data/data_source/TripDatabase.kt
+++ app/src/main/java/com/example/explorersos/feature_note/data/data_source/TripDatabase.kt
@@ -1,4 +1,19 @@
 package com.example.explorersos.feature_note.data.data_source
 
-class TripDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.explorersos.feature_note.domain.model.Alert
+import com.example.explorersos.feature_note.domain.model.AlertRecipient
+import com.example.explorersos.feature_note.domain.model.Trip
+
+@Database(
+    entities = [Trip::class, Alert::class, AlertRecipient::class],
+    version = 1
+)
+abstract class TripDatabase : RoomDatabase() {
+    abstract val tripDao: TripDao
+
+    companion object {
+        const val DATABASE_NAME = "trips_db"
+    }
 }
