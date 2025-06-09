1095ddd101db5b35e6fd4ed0c1f5776ed18f0c6f
Hugo <hugorosa123@gmail.com>
2025-06-09 09:38:49 -0600

    Implemented AlertRecipientTest.kt

--- app/src/test/java/com/example/explorersos/feature_note/domain/model/AlertRecipientTest.kt
+++ app/src/test/java/com/example/explorersos/feature_note/domain/model/AlertRecipientTest.kt
@@ -1,9 +1,9 @@
 package com.example.explorersos.feature_note.domain.model
 
-import junit.framework.TestCase.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
 
 class AlertRecipientTest {
 
