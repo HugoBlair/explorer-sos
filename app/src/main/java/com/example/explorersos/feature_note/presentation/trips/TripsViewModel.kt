17adead4f784e90b2e52e3e9b4c8776da232ba26
Hugo <hugorosa123@gmail.com>
2025-06-09 20:27:52 -0600

    Implemented TripsViewModel.kt

--- app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsViewModel.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsViewModel.kt
@@ -1,4 +1,71 @@
 package com.example.explorersos.feature_note.presentation.trips
 
-class TripsViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.explorersos.feature_note.domain.model.Trip
+import com.example.explorersos.feature_note.domain.use_case.TripUseCases
+import com.example.explorersos.feature_note.domain.util.OrderType
+import com.example.explorersos.feature_note.domain.util.TripOrder
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class TripsViewModel(private val tripUseCases: TripUseCases) : ViewModel() {
+    private val _state = mutableStateOf(TripsState())
+    val state: State<TripsState> = _state
+
+    private var recentlyDeletedTrip: Trip? = null
+    private var getTripsJob: Job? = null
+
+    init {
+        getTrips(TripOrder.Date(OrderType.Descending))
+    }
+
+    fun onEvent(event: TripsEvent) {
+        when (event) {
+            is TripsEvent.Order -> {
+                if (state.value.tripOrder::class == event.tripOrder::class &&
+                    state.value.tripOrder.orderType == event.tripOrder.orderType
+                ) {
+                    return
+                }
+                getTrips(event.tripOrder)
+            }
+
+            is TripsEvent.DeleteTrip -> {
+                viewModelScope.launch {
+                    tripUseCases.deleteTrip(event.trip)
+                    recentlyDeletedTrip = event.trip
+                }
+            }
+
+            is TripsEvent.RestoreTrip -> {
+                viewModelScope.launch {
+                    tripUseCases.addTrip(recentlyDeletedTrip ?: return@launch)
+                    recentlyDeletedTrip = null
+                }
+            }
+
+            is TripsEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+        }
+    }
+
+    private fun getTrips(tripOrder: TripOrder) {
+        getTripsJob?.cancel()
+        tripUseCases.getTrips(tripOrder).onEach { trips ->
+            _state.value = state.value.copy(
+                trips = trips,
+                tripOrder = tripOrder
+            )
+        }.launchIn(viewModelScope)
+
+    }
+
 }
