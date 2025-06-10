29da008d1d6e03b4a5e31bd75eba9fa4a3e76137
Hugo <hugorosa123@gmail.com>
2025-06-09 20:26:50 -0600

    Implemented TripsEvent.kt

--- app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsEvent.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsEvent.kt
@@ -1,4 +1,12 @@
 package com.example.explorersos.feature_note.presentation.trips
 
-class TripsEvent {
+import com.example.explorersos.feature_note.domain.model.Trip
+import com.example.explorersos.feature_note.domain.util.TripOrder
+
+sealed class TripsEvent {
+    data class Order(val tripOrder: TripOrder) : TripsEvent()
+    data class DeleteTrip(val trip: Trip) : TripsEvent()
+    object RestoreTrip : TripsEvent()
+    object ToggleOrderSection : TripsEvent()
+
 }
