61639bec6098e310e7938451d33f03f7684567cb
Hugo <hugorosa123@gmail.com>
2025-06-09 20:27:04 -0600

    Implemented TripsState.kt

--- app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsState.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/trips/TripsState.kt
@@ -1,4 +1,11 @@
 package com.example.explorersos.feature_note.presentation.trips
 
-class TripsState {
-}
+import com.example.explorersos.feature_note.domain.model.Trip
+import com.example.explorersos.feature_note.domain.util.OrderType
+import com.example.explorersos.feature_note.domain.util.TripOrder
+
+data class TripsState(
+    val trips: List<Trip> = emptyList(),
+    val tripOrder: TripOrder = TripOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+)
