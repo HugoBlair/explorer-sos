7de8a2d9dffe29090dadaecf2615d63fbc39d370
Hugo <hugorosa123@gmail.com>
2025-06-13 21:12:35 -0600

    Deleted default greeting in MainActivity.kt

--- app/src/main/java/com/example/explorersos/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.explorersos.feature_note.domain.model.Trip
 import com.example.explorersos.feature_note.presentation.trips.components.TripItem
 import com.example.explorersos.ui.theme.ExplorerSOSTheme
@@ -43,21 +40,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExplorerSOSTheme {
-        Greeting("Android")
     }
 }
