3e5f77ae7a7a558679e535e8f506ad3a4084d82b
Hugo <hugorosa123@gmail.com>
2025-06-16 16:26:40 -0600

    Added TripsViewModel to Dependency Injection

--- app/src/main/java/com/example/explorersos/di/AppModule.kt
+++ app/src/main/java/com/example/explorersos/di/AppModule.kt
@@ -10,6 +10,7 @@ import com.example.explorersos.feature_note.domain.use_case.DeleteTrip
 import com.example.explorersos.feature_note.domain.use_case.GetTrip
 import com.example.explorersos.feature_note.domain.use_case.GetTrips
 import com.example.explorersos.feature_note.domain.use_case.TripUseCases
+import com.example.explorersos.feature_note.presentation.trips.TripsViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
@@ -28,6 +29,8 @@ val appModule = module {
     }
 
     single<TripRepository> { TripRepositoryImpl(get<TripDao>()) }
+
+    single<TripsViewModel> { TripsViewModel(get<TripUseCases>()) }
 
     single<TripUseCases> {
         TripUseCases(
