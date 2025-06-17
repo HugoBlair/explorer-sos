7da3887db77bb34657be84fe4357d07398849a93
Hugo <hugorosa123@gmail.com>
2025-06-16 18:11:13 -0600

    Adjusted navigation

--- app/src/main/java/com/example/explorersos/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/MainActivity.kt
@@ -4,51 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.explorersos.feature_note.domain.model.Trip
-import com.example.explorersos.feature_note.presentation.trips.components.TripItem
+import androidx.navigation.compose.rememberNavController
+import com.example.explorersos.feature_note.presentation.navigation.AppNavigation
 import com.example.explorersos.ui.theme.ExplorerSOSTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             ExplorerSOSTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TripItem(
-                        Trip(
-                            id = 1,
-                            title = "Big Climb",
-                            destination = "Mt Kaukau",
-                            startDate = "2025-01-01T00:00:00Z",
-                            endDate = "2025-01-02T00:00:00Z",
-                            createdAt = "2024-12-31T23:59:59Z"
-                        ),
-                        modifier = Modifier.padding(innerPadding),
-                        onClick = {}
+                val navController = rememberNavController()
+                AppNavigation(
+                    navController = navController,
+                    modifier = Modifier.fillMaxSize()
+                )
 
 
-                    )
-                    TripItem(
-                        Trip(
-                            id = 1,
-                            title = "Big Climb",
-                            destination = "Mt Kaukau",
-                            startDate = "2025-01-01T00:00:00Z",
-                            endDate = "2025-01-02T00:00:00Z",
-                            createdAt = "2024-12-31T23:59:59Z"
-                        ),
-                        modifier = Modifier.padding(innerPadding),
-                        onClick = {}
-
-
-                    )
-
-                }
             }
         }
     }
--- app/src/main/java/com/example/explorersos/feature_note/presentation/navigation/Routes.kt
+++ app/src/main/java/com/example/explorersos/feature_note/presentation/navigation/Routes.kt
@@ -2,11 +2,10 @@ package com.example.explorersos.feature_note.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-object NotesScreenRoute
+sealed class Routes {
 
-@Serializable
-data class AddEditNoteScreenRoute(
-    val noteId: Int = -1, // Use a default value to make it optional
-    val noteColor: Int = -1
-)
+    @Serializable
+    data object TripsScreenRoute : Routes()
+
+    data class AddEditTripScreenRoute(val tripId: Int? = null) : Routes()
+}
