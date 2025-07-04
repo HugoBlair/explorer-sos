5f7ec66a174765cf7bd033daadf0e108675afb38
Hugo <hugorosa123@gmail.com>
2025-07-03 19:20:36 -0600

    Moved default description

--- app/src/main/java/com/example/explorersos/feature_note/domain/model/Trip.kt
+++ app/src/main/java/com/example/explorersos/feature_note/domain/model/Trip.kt
@@ -15,18 +15,21 @@ data class Trip(
     val startDateTime: Instant,
     val expectedEndDateTime: Instant,
     val isActive: Boolean = false,
-    val description: String = "I am ${if (isActive) "currently on" else "planning"}" +
-            " the \"$title\" trip from $startLocation to $endLocation . I am planning to leave on ${
-                getFormattedDisplayTime(
-                    startDateTime
-                )
-            }" +
-            " and return on ${getFormattedDisplayTime(expectedEndDateTime)}.",
+    val description: String = "",
     val createdAt: String = ZonedDateTime.now().toString(),
     val alertId: Int? = null // FK to Alert table
 
 ) {
     init {
+        if (description == "" || description.isBlank()) {
+            "I am ${if (isActive) "currently on" else "planning"}" +
+                    " the \"$title\" trip from $startLocation to $endLocation . I am planning to leave on ${
+                        getFormattedDisplayTime(
+                            startDateTime
+                        )
+                    }" +
+                    " and return on ${getFormattedDisplayTime(expectedEndDateTime)}."
+        }
         if (title.isBlank()) {
             throw InvalidTripException("Title cannot be blank.")
         }
