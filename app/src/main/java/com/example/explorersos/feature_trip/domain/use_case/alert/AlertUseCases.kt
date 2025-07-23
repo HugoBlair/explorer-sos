56fc570547ecf97b0f9d182411e9f93e91fc3b46
Hugo <hugorosa123@gmail.com>
2025-07-23 15:32:17 -0600

    Implemented AlertUseCases.kt

---
+++ app/src/main/java/com/example/explorersos/feature_trip/domain/use_case/alert/AlertUseCases.kt
@@ -0,0 +1,7 @@
+package com.example.explorersos.feature_trip.domain.use_case.alert
+
+data class AlertUseCases(
+    val deleteAlert: DeleteAlert,
+    val addAlert: AddAlert,
+    val getAlert: GetAlert
+)
