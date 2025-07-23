71a69812304414cb9f990c5a361457dfd3ede6f4
Hugo <hugorosa123@gmail.com>
2025-07-23 17:38:45 -0600

    Added @Entity

--- app/src/main/java/com/example/explorersos/feature_trip/domain/model/CheckIn.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/domain/model/CheckIn.kt
@@ -1,9 +1,21 @@
 package com.example.explorersos.feature_trip.domain.model
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.example.explorersos.feature_trip.domain.util.CheckInStatus
 import java.time.Instant
 
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Trip::class,
+            parentColumns = ["id"],
+            childColumns = ["tripId"],
+            onDelete = ForeignKey.CASCADE // If a trip is deleted, its check-ins are also deleted.
+        )
+    ]
+)
 data class CheckIn(
     @PrimaryKey(autoGenerate = true) val id: Int? = null,
     val tripId: Int, // The foreign key linking it to a Trip
