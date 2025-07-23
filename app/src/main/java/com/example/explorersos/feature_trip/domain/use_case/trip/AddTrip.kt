c7e8ac9eb31eed4c7fa699c11b24af83cd81741d
Hugo <hugorosa123@gmail.com>
2025-07-23 15:26:41 -0600

    Removed redundant validation

--- app/src/main/java/com/example/explorersos/feature_trip/domain/use_case/trip/AddTrip.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/domain/use_case/trip/AddTrip.kt
@@ -8,13 +8,6 @@ class AddTrip(
 ) {
     @Throws(Trip.InvalidTripException::class)
     suspend operator fun invoke(trip: Trip) {
-        if (trip.title.isBlank()) {
-            throw Trip.InvalidTripException("The title of the trip can't be empty.")
-        }
-        if (trip.description.isBlank()) {
-            throw Trip.InvalidTripException("The content of the trip can't be empty.")
-
-        }
         repository.insertTrip(trip)
     }
 }
