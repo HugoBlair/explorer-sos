b6de6f9a294d2860fd7d71a032e253543bf317a0
Hugo <hugorosa123@gmail.com>
2025-07-23 15:57:57 -0600

    Implemented AlertRecipientDAO.kt

---
+++ app/src/main/java/com/example/explorersos/feature_trip/data/data_source/AlertRecipientDAO.kt
@@ -0,0 +1,24 @@
+package com.example.explorersos.feature_trip.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.explorersos.feature_trip.domain.model.AlertRecipient
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlertRecipientDAO {
+    @Query("SELECT * FROM alertRecipient")
+    fun getAlertRecipients(): Flow<List<AlertRecipient>>
+
+    @Query("SELECT * FROM alertRecipient WHERE id = :id")
+    suspend fun getAlertRecipientById(id: Int): AlertRecipient?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAlertRecipient(alertRecipient: AlertRecipient)
+
+    @Delete
+    suspend fun deleteAlertRecipient(alertRecipient: AlertRecipient)
+}
