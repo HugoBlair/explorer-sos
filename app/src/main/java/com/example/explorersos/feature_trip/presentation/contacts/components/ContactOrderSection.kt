73bc2a0834dfb52393d93bc10b12d1325efac414
Hugo <hugorosa123@gmail.com>
2025-07-26 20:59:18 -0600

    Implemented ContactOrderSection.kt

---
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/contacts/components/ContactOrderSection.kt
@@ -0,0 +1,55 @@
+package com.example.explorersos.feature_trip.presentation.contacts.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.explorersos.feature_trip.domain.util.ContactOrder
+import com.example.explorersos.feature_trip.domain.util.OrderType
+import com.example.explorersos.feature_trip.presentation.trips.components.DefaultRadioButton
+
+@Composable
+fun ContactOrderSection(
+    modifier: Modifier = Modifier,
+    contactOrder: ContactOrder = ContactOrder.LastName(OrderType.Ascending),
+    onOrderChange: (ContactOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "First Name",
+                selected = contactOrder is ContactOrder.FirstName,
+                onSelect = { onOrderChange(ContactOrder.FirstName(contactOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Last Name",
+                selected = contactOrder is ContactOrder.LastName,
+                onSelect = { onOrderChange(ContactOrder.LastName(contactOrder.orderType)) }
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Ascending",
+                selected = contactOrder.orderType is OrderType.Ascending,
+                onSelect = { onOrderChange(contactOrder.copy(OrderType.Ascending)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Descending",
+                selected = contactOrder.orderType is OrderType.Descending,
+                onSelect = { onOrderChange(contactOrder.copy(OrderType.Descending)) }
+            )
+        }
+    }
+}
