42e8f9830c075dfeaa1ec68186c71062ea8e1633
Hugo <hugorosa123@gmail.com>
2025-07-26 20:12:12 -0600

    Implemented ContactTextFieldState.kt

---
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/add_edit_contact/ContactTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.example.explorersos.feature_trip.presentation.add_edit_contact
+
+data class ContactTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
