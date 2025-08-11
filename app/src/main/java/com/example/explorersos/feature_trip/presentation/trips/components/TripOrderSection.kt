09f2249115d351198932eafb6577ad4fe38ff800
Hugo <hugorosa123@gmail.com>
2025-08-11 15:01:18 -0600

    Added Horizontal Divider

--- app/src/main/java/com/example/explorersos/feature_trip/presentation/trips/components/TripOrderSection.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/trips/components/TripOrderSection.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.explorersos.feature_trip.domain.util.OrderType
 import com.example.explorersos.feature_trip.domain.util.TripOrder
@@ -38,6 +40,8 @@ fun TripOrderSection(
 
                 })
         }
+        HorizontalDivider(modifier, 1.dp, Color.Black)
+
         Row(
             modifier = Modifier.fillMaxWidth()
         ) {
