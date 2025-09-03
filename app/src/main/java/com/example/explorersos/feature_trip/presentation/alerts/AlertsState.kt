5ee3c28dd6d6357610eeda1a6e6ae1a817c19559
Hugo <hugorosa123@gmail.com>
2025-09-03 13:56:43 -0600

    Implemented AlertsState.kt

--- app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsState.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsState.kt
@@ -1,8 +1,11 @@
 package com.example.explorersos.feature_trip.presentation.alerts
 
 import com.example.explorersos.feature_trip.domain.model.Alert
+import com.example.explorersos.feature_trip.domain.util.AlertOrder
+import com.example.explorersos.feature_trip.domain.util.OrderType
 
-class AlertsState {
-    val alerts: List<Alert> = emptyList()
-
-}
+data class AlertsState(
+    val alerts: List<Alert> = emptyList(),
+    val alertOrder: AlertOrder = AlertOrder.Date(OrderType.Ascending),
+    val isOrderSectionVisible: Boolean = false
+)
