13c1d2400830d2648b14d3e754a84e425f474024
Hugo <hugorosa123@gmail.com>
2025-09-03 13:40:18 -0600

    Implemented AlertsState.kt

---
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsState.kt
@@ -0,0 +1,8 @@
+package com.example.explorersos.feature_trip.presentation.alerts
+
+import com.example.explorersos.feature_trip.domain.model.Alert
+
+class AlertsState {
+    val alerts: List<Alert> = emptyList()
+
+}
