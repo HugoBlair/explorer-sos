f926b906786868bfac96c1911808b4b1fdd873de
Hugo <hugorosa123@gmail.com>
2025-09-03 13:58:04 -0600

    Added ordering

--- app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsEvent.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsEvent.kt
@@ -1,8 +1,11 @@
 package com.example.explorersos.feature_trip.presentation.alerts
 
 import com.example.explorersos.feature_trip.domain.model.Alert
+import com.example.explorersos.feature_trip.domain.util.AlertOrder
 
 sealed class AlertsEvent {
+    data class Order(val alertOrder: AlertOrder) : AlertsEvent()
     data class DeleteAlert(val alert: Alert) : AlertsEvent()
     object RestoreAlert : AlertsEvent()
+    object ToggleOrderSection : AlertsEvent()
 }
