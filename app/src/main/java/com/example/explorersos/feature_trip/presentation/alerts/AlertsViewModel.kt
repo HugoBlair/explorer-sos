b85e2a34fd8b45f43d13ade6c61aa4370e1e63c9
Hugo <hugorosa123@gmail.com>
2025-09-13 17:27:16 -0600

    Added AlertsViewModel.kt

--- app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsViewModel.kt
+++ app/src/main/java/com/example/explorersos/feature_trip/presentation/alerts/AlertsViewModel.kt
@@ -6,24 +6,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.explorersos.feature_trip.domain.model.Alert
 import com.example.explorersos.feature_trip.domain.use_case.alert.AlertUseCases
+import com.example.explorersos.feature_trip.domain.use_case.contact.ContactUseCases
 import com.example.explorersos.feature_trip.domain.util.AlertOrder
 import com.example.explorersos.feature_trip.domain.util.OrderType
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class AlertsViewModel(private val alertUseCases: AlertUseCases) : ViewModel() {
+class AlertsViewModel(
+    private val alertUseCases: AlertUseCases,
+    private val contactUseCases: ContactUseCases
+) : ViewModel() {
     private val _state = mutableStateOf(AlertsState())
     val state: State<AlertsState> = _state
 
     private var recentlyDeletedAlert: Alert? = null
     private var getAlertsJob: Job? = null
 
-    private val _eventflow = MutableSharedFlow<UiEvent>()
-    val eventFlow = _eventflow.asSharedFlow()
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
 
     init {
         getAlerts(AlertOrder.Date(OrderType.Ascending))
@@ -44,9 +49,9 @@ class AlertsViewModel(private val alertUseCases: AlertUseCases) : ViewModel() {
                 viewModelScope.launch {
                     alertUseCases.deleteAlert(event.alert)
                     recentlyDeletedAlert = event.alert
-                    _eventflow.emit(
+                    _eventFlow.emit(
                         UiEvent.ShowSnackbar(
-                            message = "Alert deleted", "Undo"
+                            message = "Alert deleted", action = "Undo"
                         )
                     )
                 }
@@ -67,19 +72,28 @@ class AlertsViewModel(private val alertUseCases: AlertUseCases) : ViewModel() {
         }
     }
 
-
-    sealed class UiEvent {
-        data class ShowSnackbar(val message: String, val action: String? = null) : UiEvent()
-    }
-
     private fun getAlerts(order: AlertOrder) {
         getAlertsJob?.cancel()
         getAlertsJob = alertUseCases.getAlerts(order)
-            .onEach { alerts ->
+            .combine(contactUseCases.getContacts()) { alerts, contacts ->
+                val contactsById = contacts.associateBy { it.id }
+                alerts.map { alert ->
+                    val recipients = alert.recipientsId.mapNotNull { id ->
+                        contactsById[id]
+                    }
+                    alert to recipients
+                }
+            }
+            .onEach { alertsWithRecipients ->
                 _state.value = state.value.copy(
-                    alerts = alerts,
+                    alerts = alertsWithRecipients.map { it.first },
+                    alertsWithRecipients = alertsWithRecipients,
                     alertOrder = order
                 )
             }.launchIn(viewModelScope)
+    }
+
+    sealed class UiEvent {
+        data class ShowSnackbar(val message: String, val action: String? = null) : UiEvent()
     }
 }
